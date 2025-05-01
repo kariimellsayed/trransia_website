771d41b0fb78015c255bd27de307f53b08ed44f7
kariimellsayed <kariimellsayed@gmail.com>
2025-05-01 23:33:42 +0300

    edit whathsapp Button

--- app/[locale]/_components/ui/ContactButton.tsx
+++ app/[locale]/_components/ui/ContactButton.tsx
@@ -3,9 +3,13 @@ import { useTranslations } from "next-intl";
 
 const ContactButton = () => {
   const t = useTranslations("Button");
+  const defaultMessage = "مرحبًا، أحتاج مساعدة من خدمة العملاء";
+
   return (
     <a
-      href="https://wa.me/966544214748"
+      href={`https://wa.me/966544214748?text=${encodeURIComponent(
+        defaultMessage
+      )}`}
       target="_blank"
       rel="noopener noreferrer"
       className="border border-primary text-primary hover:bg-primary hover:text-white
